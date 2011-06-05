d26ce1be602245ce968429bbd5f23267c2c8b3f4
Barnaba Turek <barnabaturek@gmail.com>
2011-06-05 23:51:40 +0200

    range class

--- src/t3/Range.scala
+++ src/t3/Range.scala
@@ -1,5 +1,37 @@
 package t3
+import scala.math
 
-class Range {
+class KeyRange (alphabet:String, len:Int, range: Range) extends Iterator[String] {
 
+  var current : Int = 0;
+  val alen : Int = alphabet.length()
+
+  def this(alphabet:String, len:Int, step:Int) = 
+    this(alphabet, len, 0 to math.pow(alphabet.length(), len).toInt by step)
+
+  def next(): String = {
+    val value = currentString(range(current), Nil)
+    current+=1
+    value
+  }
+
+  def hasNext: Boolean = current < range.length
+
+  def pop(howMany: Int): KeyRange = {
+    val r = new KeyRange(alphabet, len, current to (current + howMany) by range.step)
+    current+=howMany
+    return r
+  }
+
+  private def currentString(current:Int, chars:List[Char]) : String = {
+    val char = alphabet(current % alen)
+    val nextChars = char :: chars
+    val next = current / alen
+
+    if (next == 0){
+      return alphabet(0).toString() * (len - nextChars.length) + nextChars.mkString("")
+    } else {
+      currentString(next, nextChars)
+    }
+  }
 }
---
+++ src/t3/test/testRange.scala
@@ -0,0 +1,30 @@
+import org.scalatest.FunSuite
+import t3.KeyRange
+
+class RangeSuite extends FunSuite {
+
+  test("next method works") {
+    val r = new KeyRange("01", 4, 1)
+    assert("0000" == r.next())
+    assert("0001" == r.next())
+    assert("0010" == r.next())
+  }
+
+  test("step in next works") {
+    val r = new KeyRange("01", 4, 4)
+    assert("0000" == r.next())
+    assert("0100" == r.next())
+    assert("1000" == r.next())
+  }
+
+  test("poped Range starting position") {
+    val r = new KeyRange("01", 4, 1)
+    r.next()
+    r.pop(10)
+    val k = r.pop(10)
+    // eleventh string
+    assert(k.next() == "1011")
+  }
+  
+}
+
